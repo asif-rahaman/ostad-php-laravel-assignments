2727ef923b5f5064feb8f71dde87a7daae79b7f4
Asif Rahaman <asif.u.rahaman@gmail.com>
2023-05-29 23:48:04 +0600

    Module 15 Assignment Submitted

---
+++ assignment-module-15/resources/views/pages/Home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('components.Hero')
+    @include('components.HowWeDo')
+    @include('components.Pricing')
+    @include('components.Team')
+    @include('components.NewsLetter')
+@endsection
